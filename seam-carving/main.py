93309bc584abff9a58d2ac6074d9124daa60a4c7
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-08-26 15:06:52 -0700

    seam carving works!!!!

--- seam-carving/main.py
+++ seam-carving/main.py
@@ -4,12 +4,15 @@
 #     "numpy",
 #     "opencv-python",
 #     "pillow",
+#     "tqdm",
 # ]
 # ///
 import numpy as np
 from PIL import Image
 import os
 import cv2
+from copy import deepcopy
+from tqdm import trange
 
 def save(img):
     im = Image.fromarray(img) # (img * 255).astype(np.uint8)
@@ -18,16 +21,20 @@ def save(img):
 
 # read image
 img = np.array(Image.open("image.png"))
-# get energies
-energies = cv2.Canny(img, 100, 200)
-print(energies.shape) # 651, 960
+# # get energies
+# energies = cv2.Canny(img, 100, 200)
+# print(energies.shape) # 651, 960
 
 
 # get list of seams (insane dynamic programming algorithm?)
 # https://en.wikipedia.org/wiki/Seam_carving#Dynamic_programming
 
 class PixelGrid:
-    def __init__(self,img):
+    def __init__(self,img,energies=None):
+        if energies == None:
+            energies = cv2.Canny(img, 100, 200)
+
+        self.energies = energies
 
         self.pixels = np.array([[Pixel(x,y,self) for x in range(len(img[y]))] for y in range(len(img))])
         self.img = img
@@ -38,7 +45,7 @@ class PixelGrid:
         return self.pixels[y][x]
     
     def populate(self):
-        for row in range(len(energies)):
+        for row in range(len(self.energies)):
             #print(row)
             if row != 0:
                 for pixel in self.pixels[row]:
@@ -57,7 +64,7 @@ class PixelGrid:
         #print(lowest_value)
         cur = lowest
         seam = []
-        for _ in range(len(energies)):
+        for _ in range(len(self.energies)):
             seam.append(cur)
             # print(cur)
             # print(cur.cumulative)
@@ -70,16 +77,37 @@ class PixelGrid:
     def visualize(self):
         img2 = self.img.copy()
         for pixel in self.seam:
-            img2[pixel.y, pixel.x] = 120
+            img2[pixel.y, pixel.x] = [255,0,255]
         
         save(img2)
+
+    def remove_seam(self):
+        img2 = self.img.copy().tolist()
+        seam_rev = self.seam[::-1]
+
+        new_rows = []
+
+        for row_idx in range(len(img2)):
+            row = deepcopy(img2[row_idx])
+            p = seam_rev[row_idx]
+            del row[p.x]
+
+            new_rows.append(row)
+        save(np.array(new_rows, dtype="uint8"))
+        return new_rows
+
+
+            
+
+        
 
 
 class Pixel: 
     def __init__(self, x, y,grid):
         self.x = x
         self.y = y
-        self.energy = energies[y][x]
+        self.energy = grid.energies[y][x]
+        self.energies = grid.energies
         self.grid = grid
 
         self.cumulative = self.energy
@@ -102,9 +130,9 @@ class Pixel:
 
         #_dummy = energies[px][py] # to get indexerror, I guess?
         #print(px,py,self.x, self.y)
-        if px < 0 or py < 0 or py >= len(energies) or px >= len(energies[0]):
+        if px < 0 or py < 0 or py >= len(self.energies) or px >= len(self.energies[0]):
             return
-        if py >= len(energies) or px >= len(energies[0]):
+        if py >= len(self.energies) or px >= len(self.energies[0]):
             return
         other = self.grid.get_pixel(px, py)
         other.receive(self)
@@ -118,8 +146,16 @@ class Pixel:
     def __repr__(self):
         return f"Pixel({self.x},{self.y})"
 
-grid = PixelGrid(energies)
+grid = PixelGrid(img)
 grid.populate()
 grid.visualize()
+new = grid.remove_seam()
+
+cur = new
+for i in trange(100):
+    grid = PixelGrid(np.array(cur, dtype="uint8"))
+    grid.populate()
+    grid.visualize()
+    cur =grid.remove_seam()
 # remove low-energy seams
 # repeat
